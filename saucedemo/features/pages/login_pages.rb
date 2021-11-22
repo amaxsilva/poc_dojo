0b35aa0c5137cbb719d91565eca99993430864b5
amaxsilva <amax.kerickson@gmail.com>
2021-11-22 14:56:03 -0400

    add elementos pagina login

--- saucedemo/features/pages/login_pages.rb
+++ saucedemo/features/pages/login_pages.rb
@@ -3,5 +3,6 @@ class LoginPage < SitePrism::Page
   element :key_login, '#user-name'
   element :key_password, '#password'
   element :btn_login, '#login-button'
-  element :msg_erro_login, :xpath, '//*[@id="login_button_container"]/div/form/div[3]/h3'  
+  element :login_sucesso, :xpath, '//*[@id="header_container"]/div[2]/span'
+  element :login_invalido, :xpath, '//*[@id="login_button_container"]/div/form/div[3]/h3'
 end
